092996ce32ba430e908705e8a8941bec90d1cb49
atulkumar7810 <atulkumarsingh7810@gmail.com>
2026-02-24 16:27:02 +0530

    Initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// ✅ Force Dark Mode By Default
+const theme = localStorage.getItem("theme");
+
+if (theme === "light") {
+  document.documentElement.classList.remove("dark");
+} else {
+  document.documentElement.classList.add("dark"); // default dark
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
